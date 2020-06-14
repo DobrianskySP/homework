83dd9a39593fadf08c56b557ee5337273e7ebc28
Станислав <dobriansky.sp@oilpc.ru>
2020-06-14 22:30:18 +0300

    all done

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,28 @@ public class Main {
         Nums<Integer> num = new Nums<>(1,2,3,4,5);
         str.changePosition(2,3);
         num.changePosition(1,5);
-
+        //-----создаем новый стринговый и переделываем в ArrayList-----------
         Nums<String> merge = new Nums<>("1","12","123","1234","12345");
         System.out.println(merge.mergeToList());
-        //--------------------------------------------------
+        //------создаем боксы с разными классами фруктов---------------------
         Box<Fruit> boxOfApple = new Box<>(new Apple());
         Box<Fruit> boxOfOrange = new Box<>(new Orange());
         Box<Fruit> secondBoxOfApple = new Box<>(new Apple());
-
+        //-----добавляем в существующие боксы разные фрукты------------------
         boxOfApple.putFruit(new Apple());
         boxOfOrange.putFruit(new Orange());
         boxOfApple.putFruit(new Orange());
+        //-----узнаем вес боксов с фруктами----------------------------------
         boxOfApple.allWeight();
         boxOfOrange.allWeight();
         secondBoxOfApple.allWeight();
-
+        //-----сравниваем боксы по весу--------------------------------------
         boxOfApple.compare(boxOfApple);
-
+        //-----пытаемся высыпать бокс с апельсинами в бокс с яблоками--------
         boxOfApple.shiftFruit(boxOfOrange);
         boxOfApple.allWeight();
-
+        //-----высыпаем бокс с яблоками в бокс с яблоками--------------------
         boxOfApple.shiftFruit(secondBoxOfApple);
         boxOfApple.allWeight();
     }
-
 }
